e8ff1baca29750b319b3bdd016d4dcb1b29deabf
Brandon Monroy Ruiz <brandonm@grupobatia.com.mx>
2024-06-27 18:01:13 -0600

    Se está optimizando FormSegAsisViewModel

--- ViewModel/FormSegAsisViewModel.cs
+++ ViewModel/FormSegAsisViewModel.cs
@@ -1,12 +1,9 @@
 ﻿using Asis_Batia.Helpers;
 using Asis_Batia.Model;
 using CommunityToolkit.Mvvm.ComponentModel;
-using Newtonsoft.Json;
+using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
 using System.Globalization;
-using System.Net;
-using System.Text;
-using System.Windows.Input;
 
 namespace Asis_Batia.ViewModel;
 
@@ -16,20 +13,18 @@ public partial class FormSegAsisViewModel : ViewModelBase, IQueryAttributable {
     bool _isBusy;
 
     int count = 0;
+    int _idPeriodo;
+    string _tipo;
+    public string _selectionRadio;
+
     public int IdCliente { get; set; }
     public string NombreCliente { get; set; }
     public int IdEmpleado { get; set; }
     public int IdInmueble { get; set; }
-    public int IdPeriodo { get; set; }
-    public string Tipo { get; set; }
     public string Lat { get; set; }
     public string Lng { get; set; }
     public string PathPhoto { get; set; }
     public string PathFile { get; set; }
-
-
-
-    public string _selectionRadio;
 
     public string SelectionRadio {
         get { return _selectionRadio; }
@@ -42,14 +37,6 @@ public partial class FormSegAsisViewModel : ViewModelBase, IQueryAttributable {
         get { return _respuestaTxt; }
         set { _respuestaTxt = value; OnPropertyChanged(); }
     }
-
-    private ObservableCollection<PeriodoNominaModel.PeriodoClient> _periodo;
-
-    public ObservableCollection<PeriodoNominaModel.PeriodoClient> Periodo {
-        get { return _periodo; }
-        set { _periodo = value; }
-    }
-
 
     public byte FileBase64 { get; set; }
     public byte Foto { get; set; }
@@ -65,40 +52,29 @@ public partial class FormSegAsisViewModel : ViewModelBase, IQueryAttributable {
 
     List<string> archivos = new List<string>();
 
-
-
-    public ICommand RegisterCommand { get; set; }
-    public ICommand LoadFileCommand { get; set; }
-    public ICommand PhotoCommand { get; set; }
-
     public FormSegAsisViewModel(IMediaPicker mediaPicker) {
-        RegisterCommand = new Command(async () => await Register());
-        LoadFileCommand = new Command(async () => await LoadFile());
-        PhotoCommand = new Command(async () => await Photo());
         this.mediaPicker = mediaPicker;
     }
 
-    public void ApplyQueryAttributes(IDictionary<string, object> query) {// aqui estamos recibiendo el diccionario que enviamos desde el form anterior
-        //IdCliente = (int)query["IdCliente"];// te fijas que una ves establecido el valor del cliente
-        //IdEmpleado = (int)query["IdEmpleado"];//lo estamos usando para hacer la peticion en el api
-        //IdInmueble = (int)query["IdInmueble"];// fijate que ya nos llegaron los datos
-        //NombreCliente = (string)query["NombreEmpleado"];
+    public void ApplyQueryAttributes(IDictionary<string, object> query) {
         Lat = (string)query["Lat"];
         Lng = (string)query["Lng"];
-    
     }
 
-    private void existeRegistro(int idEmple, DateTime fech, string movi) {
-        string sql = "";
-
-        sql = "select * from tb_empleado_asistencia_op where id_empleado=" + idEmple + " and CONVERT(DATE, fecha)='" + fech + "' and movimiento='" + movi + "'";
-    }
-
+    [RelayCommand]
     private async Task Register() {
         try {
             IsEnabled = false;
             count = 0;
             IsBusy = true;
+
+            if(_selectionRadio == null) {
+                await App.Current.MainPage.DisplayAlert("Mensaje", "Seleccione una opción de envío", "Cerrar");
+                IsEnabled = true;
+                IsBusy = false;
+                return;
+            }
+
             CultureInfo culture = new CultureInfo("es-MX");// Establece la cultura adecuada para México
             Location _location = await LocationService.GetCurrentLocation();
             double latitud = double.Parse(Lat, culture);
@@ -113,12 +89,6 @@ public partial class FormSegAsisViewModel : ViewModelBase, IQueryAttributable {
                 return;
             }
 
-            if(_selectionRadio == null) {
-                await App.Current.MainPage.DisplayAlert("Mensaje", "Seleccione una opción de envío", "Cerrar");
-                IsEnabled = true;
-                IsBusy = false;
-                return;
-            }
             if(_selectionRadio == "A") {
                 if(Math.Round(LocationService.CalcularDistancia(CurrentLocation, TargetDestination) * 1000, 2) > 400)//COMPROBAMOS QUE LA DISTANCIA NO SEA MAYOR A 100CM QUE EQUIVALE A 1 METRO, SI NECESITAS CAMBIAR LA DISTANCIA A COMPAR DEBES PONER EN CM LA DISTANCIA
                 {//EL METODO CalcularDistancia() YA ME REGRESA UN VALOR CALCULADO EN KM X LO CUAL SE DEBE CONVERTIR A METROS
@@ -152,15 +122,15 @@ public partial class FormSegAsisViewModel : ViewModelBase, IQueryAttributable {
                 }
             } else if(_selectionRadio == "N") { }
 
-            if(await SendFiles()) { } else {
-                await App.Current.MainPage.DisplayAlert("Error", "No fué posible guardar los archivos", "Cerrar");
-                IsEnabled = true;
-                IsBusy = false;
-                return;
-            }
+            //if(await SendFiles()) { } else {
+            //    await App.Current.MainPage.DisplayAlert("Error", "No fué posible guardar los archivos", "Cerrar");
+            //    IsEnabled = true;
+            //    IsBusy = false;
+            //    return;
+            //}
 
             IsEnabled = false;
-            await GetPeriodo(IdEmpleado);
+            await GetPeriodo();
             RegistroModel registroModel = new RegistroModel {
                 Adjuntos = PathFile == null ? "" : PathFile,
                 Anio = (int)DateTime.Today.Year,
@@ -169,96 +139,52 @@ public partial class FormSegAsisViewModel : ViewModelBase, IQueryAttributable {
                 Fecha = DateTime.Now,
                 Idempleado = IdEmpleado,
                 Idinmueble = IdInmueble,
-                Idperiodo = IdPeriodo,
+                Idperiodo = _idPeriodo,
                 Latitud = _location.Latitude.ToString(),
                 Longitud = _location.Longitude.ToString(),
                 Movimiento = _selectionRadio,
                 RespuestaTexto = _respuestaTxt == null ? "" : _respuestaTxt,
-                Tipo = Tipo,
+                Tipo = _tipo,
                 Foto = PathPhoto == null ? "" : PathPhoto,
             };
 
-            Uri RequestUri = new Uri("http://singa.com.mx:5500/api/RegistroBiometa");
-            var client = new HttpClient();
-            var json = JsonConvert.SerializeObject(registroModel);
-            var contentJson = new StringContent(json, Encoding.UTF8, "application/json");
-            var response = await client.PostAsync(RequestUri, contentJson);
-            string content = await response.Content.ReadAsStringAsync();
+            int resp = await _httpHelper.PostBodyAsync<RegistroModel, int>(Constants.API_REGISTRO_BIOMETA, registroModel);
 
-            if(response.StatusCode == HttpStatusCode.OK) {
-                IsBusy = false;
-                IsEnabled = false;
-                await App.Current.MainPage.DisplayAlert("Mensaje", "Registrado correctamente", "Ok");
-                NexTPage();
-            }
-            //else if (content == "err1")
-            //{
-            //    IsBusy = false;
-            //    IsEnabled = true;
-            //    await App.Current.MainPage.DisplayAlert("Mensaje", "Su registro fue exitoso 😊", "Ok");
-            //}
-            //else
-            else {
+            if(resp == 0) {
                 IsBusy = false;
                 IsEnabled = true;
                 await App.Current.MainPage.DisplayAlert("Error", "Ocurrió un error al registrar", "Ok");
+                return;
             }
+
+            IsBusy = false;
+            IsEnabled = false;
+            await App.Current.MainPage.DisplayAlert("Mensaje", "Registrado correctamente", "Ok");
+            NexTPage();
+
         } catch(Exception ex) {
             IsBusy = false;
             await App.Current.MainPage.DisplayAlert("Error", ex.Message, "Cerrar");
         }
     }
 
-
-    // Esta función recibe dos objetos de tipo Location, que representan las coordenadas de los puntos
-    // y devuelve la distancia en metros entre ellos, usando el método CalculateDistance de la clase Location
-
-    private async void NexTPage() {// intentemos otra vez vale11111
-        var data = new Dictionary<string, object>
-        {
-                {"NombreEmpleado", NombreCliente }
-
-            };
+    private async void NexTPage() {
+        var data = new Dictionary<string, object>{
+            {"NombreEmpleado", NombreCliente }
+        };
         await Shell.Current.GoToAsync("//FormReg", true, data);
     }
 
-    private async Task GetPeriodo(int IdEmpleado) {
-        IsBusy = true;// aqui vamos a ver toda la info que estamos perdon obteniendo seria ya qu es get
-
-        // Crear una solicitud HTTP.
-        var request = new HttpRequestMessage();
-
-        // Establecer la URL de la solicitud.
-        request.RequestUri = new Uri($"http://singa.com.mx:5500/api/PeriodoNomina?Idempleado={IdEmpleado}");
-
-        // Establecer el método de la solicitud como GET.
-        request.Method = HttpMethod.Get;
-
-        // Agregar un encabezado "Accept" para indicar que se acepta JSON como respuesta.
-        request.Headers.Add("Accept", "application/json");
-
-        // Crear una nueva instancia de HttpClient.
-        var client = new HttpClient();
-
-        // Enviar la solicitud HTTP y esperar la respuesta.
-        HttpResponseMessage response = await client.SendAsync(request);
-
-        // Verificar si la respuesta tiene un estado OK (código 200).
-        if(response.StatusCode == HttpStatusCode.OK) {
-            // Leer el contenido de la respuesta como una cadena.
-            string content = await response.Content.ReadAsStringAsync();
-
-            // Deserializar el contenido JSON en una colección observable de clientes.
-            var data = JsonConvert.DeserializeObject<ObservableCollection<PeriodoNominaModel.PeriodoClient>>(content);
-
-            // Asignar la colección de clientes a la propiedad 'Clients'.
-            Periodo = data;
-            IdPeriodo = Periodo[0].id_periodo;
-            Tipo = Periodo[0].descripcion;
-            IsBusy = false;
-        }
+    async Task GetPeriodo() {
+        IsBusy = true;
+        string url = $"{Constants.API_PERIODO_NOMINA}?Idempleado={UserSession.IdEmpleado}";
+        var periodoEmpleado = await _httpHelper.GetAsync<ObservableCollection<PeriodoNominaModel.PeriodoClient>>(url);
+        _idPeriodo = periodoEmpleado[0].id_periodo;
+        _tipo = periodoEmpleado[0].descripcion;
+        IsBusy = false;
     }
 
+    [RelayCommand]
     private async Task LoadFile() {
         try {
             PickOptions options = new PickOptions();
@@ -296,6 +222,7 @@ public partial class FormSegAsisViewModel : ViewModelBase, IQueryAttributable {
         //return ImageData;
     }
 
+    [RelayCommand]
     private async Task Photo() {
         try {
             if(this.mediaPicker.IsCaptureSupported) {
@@ -324,7 +251,6 @@ public partial class FormSegAsisViewModel : ViewModelBase, IQueryAttributable {
         foreach(var item in archivos) {
             urlarch += item;
         }
-        //await App.Current.MainPage.DisplayAlert("Url que se envían al Servidor", $"{urlarch}", "Ok");
 
         UrlFiles = await UploadFiles(archivos, "Doctos");
         string[] splits = UrlFiles.Split("|");// AQUI DEBEMOS INCLUIR EL SIGNO "|" SIN ESPAICIOS
@@ -344,37 +270,21 @@ public partial class FormSegAsisViewModel : ViewModelBase, IQueryAttributable {
         return true;
     }
 
-    // Se crea una instancia de HttpClient que se puede reutilizar
-    private static readonly HttpClient client = new HttpClient();
-
-    // Este método envía una lista de archivos y un nombre de carpeta al API
     public async Task<string> UploadFiles(List<string> files, string folderName) {
-        // Se crea un objeto MultipartFormDataContent para contener los datos del formulario
-        var formData = new MultipartFormDataContent();
 
-        // Se agrega el nombre de la carpeta como un parámetro de cadena
+        var formData = new MultipartFormDataContent();
         formData.Add(new StringContent(folderName), "folderName");
 
-        // Se recorre la lista de archivos y se agregan como parámetros de archivo
         foreach(var file in files) {
-            // Se crea un objeto StreamContent a partir del contenido del archivo
             var fileContent = new StreamContent(File.OpenRead(file));
-
-            // Se agrega el contenido del archivo al formulario con el nombre del archivo
             formData.Add(fileContent, "files", Path.GetFileName(file));
         }
 
-        // Se envía una solicitud POST al API con el formulario como contenido
-        var response = await client.PostAsync("http://singa.com.mx:5500/api/FilesAsis/CargaMul", formData);
-
-        // Se verifica si la respuesta fue exitosa
-        if(response.IsSuccessStatusCode) {
-            // Se lee el contenido de la respuesta como una cadena
-            return await response.Content.ReadAsStringAsync();
-        } else {
-            // Se lanza una excepción si la respuesta fue fallida
-            await App.Current.MainPage.DisplayAlert("Error", $"La solicitud al API falló con el código {response.StatusCode}", "Cerrar");
+        string filePathList = await _httpHelper.PostMultipartAsync<string>(Constants.API_ENVIO_ARCHIVOS, formData);
+        if(string.IsNullOrWhiteSpace(filePathList)) {
+            await App.Current.MainPage.DisplayAlert("Error", $"Ocurrió un error al subir los archivos", "Cerrar");
             return null;
         }
+        return filePathList;
     }
 }
