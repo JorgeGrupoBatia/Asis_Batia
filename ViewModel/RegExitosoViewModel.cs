30a4c86d33354f2b4b4484d38ebb52b255e2250b
Brandon Monroy Ruiz <brandonm@grupobatia.com.mx>
2024-07-02 09:25:27 -0600

    Se cambió el diseño de RegExitosoModal

--- ViewModel/RegExitosoViewModel.cs
+++ ViewModel/RegExitosoViewModel.cs
@@ -1,35 +1,11 @@
-﻿using Asis_Batia.Helpers;
+﻿using CommunityToolkit.Mvvm.Input;
 using MauiPopup;
-using System.Windows.Input;
 
 namespace Asis_Batia.ViewModel;
 
-public class RegExitosoViewModel : BaseViewModel, IQueryAttributable {
-    private string _nombreCliente;
+public partial class RegExitosoViewModel : ViewModelBase {
 
-    public string NombreCliente {
-        get { return _nombreCliente; }
-        set { _nombreCliente = value; OnPropertyChanged(); }
-    }
-
-    private DateTime _fecha;
-
-    public DateTime Fecha {
-        get { return _fecha; }
-        set { _fecha = value; OnPropertyChanged(); }
-    }
-
-    public ICommand AcceptCommand { get; set; }
-
-    public RegExitosoViewModel() {
-        NombreCliente = UserSession.Empleado;
-        AcceptCommand = new Command(Accept);
-    }
-
-    public void ApplyQueryAttributes(IDictionary<string, object> query) {
-        Fecha = DateTime.Now;
-    }
-
+    [RelayCommand]
     private void Accept() {
         PopupAction.ClosePopup();
     }
