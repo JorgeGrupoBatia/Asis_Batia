2dc8a071c77dcb8e4a0e847f9d7a93ff472e3067
Brandon Monroy Ruiz <brandonm@grupobatia.com.mx>
2024-07-04 15:07:36 -0600

    Se optimizó RulesPage

--- Helpers/Constants.cs
+++ Helpers/Constants.cs
@@ -24,4 +24,23 @@ public static class Constants {
     public const string NOMENCLATURA_KEY = "Nomenclatura key";
     public const string LOCATION_KEY = "Location key";
     #endregion
-}
+
+    public const string AVISO_PRIVACIDAD = 
+        "Gracias por utilizar nuestra aplicación móvil. La protección de su privacidad es de suma importancia para nosotros. Por lo tanto, hemos desarrollado esta Política de Privacidad para que comprenda cómo recopilamos, usamos, comunicamos, divulgamos y utilizamos su información personal."
+        + "\n\nLe recomendamos encarecidamente que revise cuidadosamente esta Política de Privacidad antes de usar la aplicación o proporcionar cualquier tipo de información personal."
+        + "\n\nRecopilación de información en BIOMETA."
+        + "\n1.Datos personales:" 
+        + "\nLa aplicación recopila y utiliza datos personales como nombre, ID de empleado y periodo nominal. Estos datos se utilizan para identificarle como usuario y para lograr correctamente su registro de asistencia laboral."
+        + "\n\n2.Datos de ubicación:"
+        + "\nLa aplicación recopila y utiliza su ubicación geográfica con la dirección de área de trabajo para verificar que su registro se haga correctamente en el inmueble asignado."
+        + "\n\n3.Archivos e imágenes:"
+        + "\nLa aplicación puede acceder y manejar archivos como PDF e imágenes almacenadas en su dispositivo móvil para enviar un registro de asistencia completo y con más información."
+        + "\n\nEn concreto, Grupo Batia podrá recabar y tratar las siguientes categorías de datos personales:"
+        + "\n• Datos de identificación \n• Datos de contacto \n• Datos de navegación, dispositivos y geolocalización \n• Datos personales. "
+        + "\n\nPara cumplir con las finalidades descritas en este Aviso, Grupo Batia no trata ni solicita datos personales sensibles."
+        + "\n\nDatos personales de terceros"
+        + "\nSi usted entrega a Grupo Batia datos personales de terceros para verificación de posibles conflictos de interés, deberá informarles sobre la existencia del tratamiento de sus datos personales y el contenido de este Aviso de Privacidad. Si proporciona dicha información, " 
+        + "también manifiesta con su entrega que cuenta previamente con el consentimiento de sus titulares para proporcionar su información a Grupo Batia y que los mismos son correctos y completos."
+        + "\n\nPara utilizar nuestra aplicación y continuar con su registro, debe aceptar y autorizar que está de acuerdo y es consciente del manejo y uso de sus datos."
+        + "\n\nAgradecemos su confianza en nosotros y estamos comprometidos en proteger toda información utilizada en BIOMETA.";
+} 
--- View/RulesPage.xaml.cs
+++ View/RulesPage.xaml.cs
@@ -1,12 +1,7 @@
-using Asis_Batia.ViewModel;
-
 namespace Asis_Batia.View;
 
-public partial class RulesPage : ContentPage
-{
-	public RulesPage()
-	{
-		InitializeComponent();
-		BindingContext = new RulesPageViewModel();
+public partial class RulesPage : ContentPage {
+    public RulesPage() {
+        InitializeComponent();
     }
 }
