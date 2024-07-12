6d1195194e60c968a73a3e1d9d6f209a0c070c7d
jor_dan <123danjor@gmail.com>
2024-07-12 13:06:34 -0600

    Se modificó el FlyoutFooterTemplate  para iOS

--- Templates/FlyoutFooterTemplate.xaml.cs
+++ Templates/FlyoutFooterTemplate.xaml.cs
@@ -1,6 +1,6 @@
 namespace Asis_Batia.Templates;
 
-public partial class FlyoutFooterTemplate : HorizontalStackLayout {
+public partial class FlyoutFooterTemplate : Grid {
     public FlyoutFooterTemplate() {
         InitializeComponent();
     }
